d7d8b9e88b469fb3d2178471432c014f85be9794
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-03-07 07:38:30 +0100

    Update Solution.cpp

--- Solution.cpp
+++ Solution.cpp
@@ -5,17 +5,15 @@ using namespace std;
 
 class Solution {
     inline static int MAX_INPUT_VALUE = pow(10, 4);
-    
 public:
     vector<int> sortByBits(vector<int>& input) {
-        const size_t size = input.size();
-        for (int i = 0; i < size; i++) {
-            input[i] = input[i] + (MAX_INPUT_VALUE + 1) * countBitsOfOne(input[i]);
+        for (auto& num : input) {
+            num = num + (MAX_INPUT_VALUE + 1) * countBitsOfOne(num);
         }
         sort(input.begin(), input.end());
 
-        for (int i = 0; i < size; i++) {
-            input[i] = input[i] % (MAX_INPUT_VALUE + 1);
+        for (auto& num : input) {
+            num = num % (MAX_INPUT_VALUE + 1);
         }
         return input;
     }
